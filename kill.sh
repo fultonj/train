b07cbc69abec2721c306b0df18cc99d380333738
John Fulton <fulton@redhat.com>
2019-08-01 18:43:43 +0000

    kill.sh: remove artifacts too

--- kill.sh
+++ kill.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
+source ~/stackrc
+
 CLOUD=overcloud
-opensatck stack delete $CLOUD --wait --yes
+# delete heat stack
+openstack stack delete $CLOUD --wait --yes
+
+# delete deployment plan 
 openstack overcloud delete $CLOUD --yes
+# yes, the above also deletes the heat stack but not as quickly
+
+if [[ -e ansible.log ]]; then
+    rm -v -f ansible.log
+fi
+if [[ -d config-download ]]; then
+    rm -v -rf config-download
+fi
