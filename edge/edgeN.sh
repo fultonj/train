0f224aaa1701afc9c0bc8addc4e630cfef09a5a3
John Fulton <fulton@redhat.com>
2019-08-28 21:09:47 +0000

    edgeN.sh can now optionally update ironic

--- edge/edgeN.sh
+++ edge/edgeN.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+IRONIC=1
+
 if [[ -z $1 ]]; then
     N=1
 else
@@ -8,6 +10,22 @@ fi
 if [[ $N -lt 1 ]]; then
     echo "Only positive integers please"
     exit 1
+fi
+
+if [[ $IRONIC -eq 1 ]]; then
+    source ~/stackrc
+    i=1
+    for UUID in $(openstack baremetal node list -f value -c UUID -c Name \
+                            -c "Provisioning State" \
+                      | grep available | grep ceph | awk {'print $1'}); do
+        openstack baremetal node set $UUID \
+                  --property capabilities="node:$i-ceph-0,boot_option:local"
+        i=$((i+1))
+    done
+    if [[ $i -ne $N ]]; then
+        echo "N being reset from $N to $i (based on available Ceph nodes)"
+        N=$i
+    fi
 fi
 
 for i in $(seq 1 $N); do
@@ -23,6 +41,7 @@ for i in $(seq 1 $N); do
     sed s/edge0/$deploy/g -i $deploy/ceph.yaml
     cp edge0/overrides.yaml $deploy/overrides.yaml
     sed s/edge0/$deploy/g -i $deploy/overrides.yaml
+    sed s/"0-ceph-%index%"/"$i-ceph-%index%"/g -i $deploy/overrides.yaml
     cp edge0/deploy.sh $deploy/deploy.sh
     sed s/edge0/$deploy/g -i $deploy/deploy.sh
     pushd $deploy
